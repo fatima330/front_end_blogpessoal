f524e3d6589069767dcd1ccf072113731e560a97
Fatima Oliveira <fatimaolliveira428@gmail.com>
2025-05-21 16:33:07 -0300

    adicionando Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Home from './pages/home/Home'
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Home/>
-  )
+    <>
+      <BrowserRouter>
+          <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+          <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App
+export default App;
